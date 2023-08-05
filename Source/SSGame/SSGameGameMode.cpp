f1ea6dbf3318066089033fbb91b119255e3b1314
linkerzhang <linkerzhang@tencent.com>
2023-08-05 20:49:21 +0800

    调整粒子流体参数，修复控制杆bug

--- Source/SSGame/SSGameGameMode.cpp
+++ Source/SSGame/SSGameGameMode.cpp
@@ -7,7 +7,7 @@
 ASSGameGameMode::ASSGameGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/BP_Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
